f7612c0f9cccce35857d0983ca1c01522f7f325e
Hossein Asgarpour <asgarpour_hossein@yahoo.com>
2020-07-10 14:07:34 +0430

    dasdsa

---
+++ FbiInquiry.Web/Areas/ApiFbi/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace FbiInquiry.Web.Areas.ApiFbi.Controllers
+{
+    [Route("OrderService")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private IConfiguration Config { get; }
+        public OrderController(IConfiguration Configuration)
+        {
+            Config = Configuration;
+            //Configuration.GetConnectionString("CivilRegistery")
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromForm] string Key)
+        {
+            
+
+            return Ok(new
+            {
+                title = "دریافت توکن",
+                status = 0,
+                message = "دریافت موفق",
+                token = ""
+            });
+        }
+    }
+}
